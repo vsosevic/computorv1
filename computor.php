4c2b1a9b8f6a3d452a689c8a3ad26476738ae17a
Vladimir Sosevich <sosevich.v@gmail.com>
2018-07-28 18:53:47 +0300

    Almost correct regular expression

--- computor.php
+++ computor.php
@@ -10,8 +10,15 @@ $poly = [
 	'degree' => 2,
 ];
 
-echo (int) $poly["sign"] *  (float) $poly["coef"];
+// echo (int) $poly["sign"] *  (float) $poly["coef"];
 
+$poly = "5 * X^0 -d 4 * X^1 9666.663 * X^2 = 1 * X^0
+8 * X^0 - 6 * X^1 + 0 * X^2 - 66665.6 * X^3 = 3 * X^0
+ 5 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 9 * x^0 + 9";
+
+$test = preg_match('/([+|-]?[ ]?\d*[\.]?\d*[ |]\*[ |][x|X]\^\d)|([+|-]?[ ]?\d)/', $poly);
+
+var_dump($test);
 
 function has_args_errors($argv) {
 
