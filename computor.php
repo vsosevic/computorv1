5d7b9ac39ef0a388a61eb571572822b8370dbcee
Vladimir Sosevich <sosevich.v@gmail.com>
2018-07-29 13:42:56 +0300

    Add .gitignore

--- computor.php
+++ computor.php
@@ -16,6 +16,8 @@ $poly = "5 * X^0 -d 4 * X^1 9666.663 * X^2 = 1 * X^0
 8 * X^0 - 6 * X^1 + 0 * X^2 - 66665.6 * X^3 = 3 * X^0
  5 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 9 * x^0 + 9";
 
+ $poly2 = "5 + 4 * X + X^2= X^2";
+
 $test = preg_match('/([+|-]?[ ]?\d*[\.]?\d*[ |]\*[ |][x|X]\^\d)|([+|-]?[ ]?\d)/', $poly);
 
 var_dump($test);
@@ -32,6 +34,10 @@ function has_args_errors($argv) {
 		return FALSE;
 	}
 
+}
+
+function has_one_equal_sign($argv) {
+	
 }
 
 ?>
