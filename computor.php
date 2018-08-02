b22b6c1b3757f183ab2e941088de003199c44225
Vladimir Sosevich <sosevich.v@gmail.com>
2018-08-03 00:10:43 +0300

    Some structure changes. Fix a bug with undefined var

--- computor.php
+++ computor.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 
 const TERM_REGULAR_PATTERN = '/([+|-]?([ ]?\d*[\.]?\d*[ ]?(\*)?[ ]?)?(([x|X]\^\d*[ ]?)|([x|X])))|([+|-]?[ ]?\d*)/';
 
 // >>> This section should be deleted!!! >>>
-$poly = "5 - 3x + 10x^2    = 0";
+// TODO: handle '0 = 0' equation.
+$poly = "3x    =3x";
 
 $argv = $poly;
 
@@ -19,7 +20,7 @@ $argv = $poly;
 function argv_has_errors($argv) {
 
     $error_message = '';
-	// More than 1 argv supplied
+    // More than 1 argv supplied
 //	if (count($argv) > 2 || count($argv) < 2) {
 //		echo "Too many or too few arguments" . PHP_EOL;
 //		echo "Usage: php computor.php \"equation\"" . PHP_EOL;
@@ -29,32 +30,26 @@ function argv_has_errors($argv) {
 
     $poly = $argv; // $argv[1] - in the future for command line
 
-	// More than one '=' signs
+    // More than one '=' signs
     $count_equals = substr_count($poly, '=');
-	if($count_equals != 1) {
+    if($count_equals != 1) {
         $error_message .= "Wrong number of equals. Should be only 1." . PHP_EOL;
-	}
+    }
 
-	// One side of equation has no arguments
-	$exploded_parts = explode('=', $poly);
+    // One side of equation has no arguments
+    $exploded_parts = explode('=', $poly);
 
-	if (!isset($exploded_parts[0]) || !isset($exploded_parts[1])) {
+    if (!isset($exploded_parts[0]) || !isset($exploded_parts[1])) {
         $error_message .= "Equation should contain smth on each side." . PHP_EOL;
-	}
+    }
 
-	// Formatting is wrong.
+    // Formatting is wrong.
     $left_part_cleaned = trim(preg_replace(TERM_REGULAR_PATTERN, '',$exploded_parts[0]));
     $right_part_cleaned = trim(preg_replace(TERM_REGULAR_PATTERN, '',$exploded_parts[1]));
 
     if (!empty($left_part_cleaned) || !empty($right_part_cleaned)) {
         $error_message .= "Something wrong with equation." . PHP_EOL;
-	}
-
-	//Degree is too high.
-    preg_match_all('/\^\d*/', $poly, $poly_degrees_dirty);
-
-    $poly_degrees_clean = str_replace('^', '', $poly_degrees_dirty[0]);
-    $max_poly_degree = max($poly_degrees_clean);
+    }
 
     if (!empty($error_message)) {
         echo $error_message;
@@ -62,19 +57,25 @@ function argv_has_errors($argv) {
         return TRUE;
     }
 
+    //Degree is too high.
+    preg_match_all('/\^\d*/', $poly, $poly_degrees_dirty);
+
+    $poly_degrees_clean = str_replace('^', '', $poly_degrees_dirty[0]);
+    $max_poly_degree = !empty($max_poly_degree) ? max($poly_degrees_clean) : 0;
+
     if ($max_poly_degree > 2) {
         // TODO: print Reduced form
         echo "Polynomial degree: $max_poly_degree" . PHP_EOL;
-        echo "The polynomial degree is stricly greater than 2, I can't solve." . PHP_EOL;
+        echo "The polynomial degree is stricly greater than 2. I can't solve." . PHP_EOL;
 
         return TRUE;
     }
 
-	return FALSE;
+    return FALSE;
 }
 
 if (!argv_has_errors($argv)) {
-	$poly = strtoupper(str_replace(' ', '', $argv)); // $argv[1] - in the future for command line
+    $poly = strtoupper(str_replace(' ', '', $argv)); // $argv[1] - in the future for command line
     $poly_exploded = explode('=', $poly);
 
     $poly_left = $poly_exploded[0];
@@ -82,22 +83,22 @@ if (!argv_has_errors($argv)) {
 
     $left_terms = create_reduced_poly_array($poly_left);
     $right_terms = create_reduced_poly_array($poly_right);
-  
+
     $reduced_leftside_terms_array = add_two_terms_arrays($left_terms, $right_terms);
 
-    print_terms_array($reduced_leftside_terms_array);
+    print_terms_array($reduced_leftside_terms_array, true);
 
 }
 
 function create_reduced_poly_array($poly) {
-  
+
     $terms_array_raw = [];
-    
+
     preg_match_all(TERM_REGULAR_PATTERN, $poly,$terms_array_raw);
 
     $terms_array_raw = array_filter($terms_array_raw[0]);
-  
-  $term_array_reduced = [];
+
+    $term_array_reduced = [];
 
     foreach ($terms_array_raw as $term) {
         $current_coef = 0;
@@ -109,27 +110,27 @@ function create_reduced_poly_array($poly) {
 
         //Define degree
         preg_match('/X[\^]?(\d+)?/', $term,$matched_degree); // searching for X ^ >>>2<<<
-        
+
         if (!empty($matched_degree)) {
             $current_degree = isset($matched_degree[1]) ? (int) $matched_degree[1] : 1;
         }
 
         // Don't save zero terms
         if ($current_coef == 0) {
-          continue;
+            continue;
         }
-        
+
         // Reduced form or just new array.
         if (!empty($term_array_reduced[$current_degree])) {
-          $term_array_reduced[$current_degree]['coef'] += $current_coef;
+            $term_array_reduced[$current_degree]['coef'] += $current_coef;
         }
         else {
-          $term_array_reduced[$current_degree] = [
-            'coef' => $current_coef,
-            'degree' => $current_degree,
-          ];
+            $term_array_reduced[$current_degree] = [
+                'coef' => $current_coef,
+                'degree' => $current_degree,
+            ];
         }
-        
+
     }
 
     ksort($term_array_reduced);
